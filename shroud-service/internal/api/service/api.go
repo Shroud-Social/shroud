0cf4b4e65a392e697f1c966023b38ec831c9164f
benji <benjamin6903@riseup.net>
2026-04-11 18:21:35 +0100

    implement_subject_builder

--- shroud-service/internal/api/service/api.go
+++ shroud-service/internal/api/service/api.go
@@ -2,59 +2,116 @@ package service
 
 import (
 	"fmt"
-	servicev1 "services/internal/api/service/v1"
-	"services/internal/comm/pubsub"
-
-	"github.com/nats-io/nats.go"
+	"log"
+	"strings"
 )
+
+const apiVersion = 1
+
+type Scope string
 
 const (
-	ApiVersion1 = "v1"
-
-	service              = ApiVersion1 + ".service"
-	messages             = service + ".message"
-	SubjectMessageNew    = messages + ".new"
-	SubjectMessageUpdate = messages + ".update"
-	SubjectMessageDelete = messages + ".delete"
-
-	uploads             = service + ".uploads"
-	SubjectUploadNew    = uploads + ".new"
-	SubjectUploadDelete = uploads + ".delete"
+	ScopeEvent   Scope = "evt"
+	ScopeLogging Scope = "log"
+	ScopeService Scope = "svc"
+	ScopeSystem  Scope = "sys"
 )
+
+type Action string
 
 const (
-	event = ApiVersion1 + ".event"
+	ActionCreate Action = "create"
+	ActionDelete Action = "delete"
+	ActionUpdate Action = "update"
 )
 
-func SubjectMessageCreated(guildId, channelId string) string {
-	return fmt.Sprintf("%s.guild.%s.channel.%s.message.created", event, guildId, channelId)
-}
+// SubjectId represents a specific entity identifier or a NATS wildcard (* or >)
+type SubjectId string
 
-func SubjectMessageUpdated(guildId, channelId string) string {
-	return fmt.Sprintf("%s.guild.%s.channel.%s.message.updated", event, guildId, channelId)
-}
-
-func SubjectMessageDeleted(guildId, channelId string) string {
-	return fmt.Sprintf("%s.guild.%s.channel.%s.message.deleted", event, guildId, channelId)
-}
-
-var (
-	SubscriptionsDispatcher = map[string]nats.MsgHandler{
-		SubjectUploadNew:     servicev1.NewUpload,
-		SubjectUploadDelete:  servicev1.DeleteUpload,
-		SubjectMessageNew:    servicev1.NewMessage,
-		SubjectMessageUpdate: servicev1.UpdateMessage,
-		SubjectMessageDelete: servicev1.DeleteMessage,
-	}
-	SubscriptionsScribe = map[string]nats.MsgHandler{}
+const (
+	Any SubjectId = "*" // Matches any single token
+	All SubjectId = ">" // Matches all subsequent tokens
 )
 
-func Subscribe(group map[string]nats.MsgHandler) error {
-	for s, h := range group {
-		_, err := pubsub.Connection.Subscribe(s, h)
-		if err != nil {
-			return err
-		}
+type SubjectBuilder struct {
+	tokens       []string
+	isTerminated bool
+}
+
+func NewSubjectBuilder(scope Scope) *SubjectBuilder {
+	return &SubjectBuilder{
+		tokens: []string{fmt.Sprintf("v%d", apiVersion), string(scope)},
 	}
-	return nil
+}
+
+func (b *SubjectBuilder) addEntity(entity string, id SubjectId) *SubjectBuilder {
+	if b.isTerminated {
+		log.Printf("SubjectBuilder: tried to add to a terminated subject")
+		return b
+	}
+	b.tokens = append(b.tokens, fmt.Sprintf("%s.%s", entity, id))
+	if id == All {
+		b.isTerminated = true
+	}
+	return b
+}
+
+func (b *SubjectBuilder) Guild(id SubjectId) *SubjectBuilder {
+	b.addEntity("guild", id)
+	return b
+}
+
+func (b *SubjectBuilder) Channel(id SubjectId) *SubjectBuilder {
+	b.addEntity("channel", id)
+	return b
+}
+
+func (b *SubjectBuilder) Message(id SubjectId) *SubjectBuilder {
+	b.addEntity("message", id)
+	return b
+}
+
+func (b *SubjectBuilder) User(id SubjectId) *SubjectBuilder {
+	b.addEntity("user", id)
+	return b
+}
+
+func (b *SubjectBuilder) Role(id SubjectId) *SubjectBuilder {
+	b.addEntity("role", id)
+	return b
+}
+
+func (b *SubjectBuilder) Upload(id SubjectId) *SubjectBuilder {
+	b.addEntity("upload", id)
+	return b
+}
+
+func (b *SubjectBuilder) Relationship(id SubjectId) *SubjectBuilder {
+	b.addEntity("relationship", id)
+	return b
+}
+
+func (b *SubjectBuilder) Any(id SubjectId) *SubjectBuilder {
+	b.addEntity("*", id)
+	return b
+}
+
+func (b *SubjectBuilder) All() *SubjectBuilder {
+	b.tokens = append(b.tokens, ">")
+	b.isTerminated = true
+	return b
+}
+
+func (b *SubjectBuilder) AddAction(action Action) *SubjectBuilder {
+	if b.isTerminated {
+		log.Printf("SubjectBuilder: tried to add to a terminated subject")
+		return b
+	}
+	b.tokens = append(b.tokens, string(action))
+	b.isTerminated = true
+	return b
+}
+
+func (b *SubjectBuilder) Build() string {
+	return strings.Join(b.tokens, ".")
 }
